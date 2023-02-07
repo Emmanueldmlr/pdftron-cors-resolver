aab778e1e8ade762bfe14bbdcb7936b065b7fbf0
Damilare Bakare <damilarebakare@Damilares-MBP.lan>
2023-02-07 13:42:59 +0100

    updates env

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const path = require('path');
 
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-const PORT = process.env.PORT || 3000;
-const NODE_PORT = process.env.REACT_APP_NODE_PORT || 3100;
-const REACT_HOST = process.env.REACT_APP_HOST || 'localhost';
+const PORT = process.env.NODE_PORT || 3100;
+const HOST = process.env.APP_HOST || 'localhost';
 
 const OPTIONS = {
-  SERVER_ROOT: `http://${REACT_HOST}`,
-  PORT: NODE_PORT,
-  CORS_OPTIONS: { origin: `http://${REACT_HOST}:${PORT}`, credentials: true },
+  SERVER_ROOT: `${HOST}`,
+  PORT: PORT,
   ALLOW_HTTP_PROXY: true,
 };
 
