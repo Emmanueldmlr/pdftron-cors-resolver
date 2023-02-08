c255bdceb0edb59dedc59d6a1ec871b075cbd928
Damilare Bakare <damilarebakare@Damilares-MBP.lan>
2023-02-08 01:16:21 +0100

    reworks set up

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const path = require('path');
 
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-const PORT = process.env.NODE_PORT || 3000;
-const HOST = process.env.APP_HOST || 'localhost';
+const PORT = process.env.PORT || 3000;
+const NODE_PORT = process.env.REACT_APP_NODE_PORT || 3100;
+const REACT_HOST = process.env.REACT_APP_HOST || 'localhost';
+
+const host = "https://mosaiq.herokuapp.com/";
+const port = process.env.PORT || 3000;
 
 const OPTIONS = {
-  SERVER_ROOT: `${HOST}`,
-  PORT: PORT,
+  SERVER_ROOT: `${host}`,
+  PORT: port,
+  CORS_OPTIONS: { origin: `${host}:${PORT}`, credentials: true },
   ALLOW_HTTP_PROXY: true,
 };
 
