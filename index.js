b1bbdd8f064dae9249f3e9f31d5082715bfb5abd
Damilare Bakare <damilarebakare@Damilares-MBP.lan>
2023-02-10 07:54:37 +0100

    fixes error

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const HTMLProxyServer = require('@pdftron/webviewer-html-proxy-server');
+const HTMLProxyServer = require('@drebakare/webviewer-html-proxy-server');
 const path = require('path');
 
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
