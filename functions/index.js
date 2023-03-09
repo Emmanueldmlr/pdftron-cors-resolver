98184701ac2e0830773b5d436c356a6849f7bad5
Damilare Bakare <bakaredmlr@gmail.com>
2023-03-09 17:51:07 +0100

    updates whitelist

--- functions/index.js
+++ functions/index.js
@@ -2,27 +2,32 @@ const functions = require("firebase-functions");
 const HTMLProxyServer = require('@drebakare/webviewer-html-proxy-server');
 const path = require('path');
 
+
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-const host = "localhost";
-const port = 9000;
-
+const host = "https://us-central1-pdftron-proxy-server.cloudfunctions.net/app";
+const port = process.env.PORT || 3000;
 
 const whitelist = [
   "http://localhost:3000",
   "https://mosaiqlabsdemo.netlify.app",
-  
+  "https://sheldon-326500.ew.r.appspot.com",
 ];
+
+const runtimeOpts = {
+  timeoutSeconds: 500,
+  memory: "2GB",
+};
 
 const OPTIONS = {
   SERVER_ROOT: host,
-  PORT: port,
+  PORT: 3000,
   CORS_OPTIONS: { origin: whitelist, credentials: true },
   COOKIE_SETTING: { sameSite: 'none', secure: true },
   ALLOW_HTTP_PROXY: false,
 };
 
-exports.app = functions.https.onRequest(HTMLProxyServer.createServer(OPTIONS));
+exports.app = functions.runWith(runtimeOpts).https.onRequest(HTMLProxyServer.createServer(OPTIONS));
 
 
 
